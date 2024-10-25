589c1a1410ff6d66e2286fdba41d04b1eda9450c
LANLightning <lllightning1@outlook.com>
2024-10-25 12:54:38 +0800

    init

---
+++ Hazel/src/Hazel/Scene/Scene.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "Hazel/Core/Timestep.h"
+#include "Hazel/Core/UUID.h"
+#include "Hazel/Renderer/EditorCamera.h"
+
+#include "entt.hpp"
+
+class b2World;
+
+namespace Hazel {
+
+	class Entity;
+
+	class Scene
+	{
+	public:
+		Scene();
+		~Scene();
+
+		Entity CreateEntity(const std::string& name = std::string());
+		Entity CreateEntityWithUUID(UUID uuid, const std::string& name = std::string());
+		void DestroyEntity(Entity entity);
+
+		void OnRuntimeStart();
+		void OnRuntimeStop();
+
+		void OnUpdateRuntime(Timestep ts);
+		void OnUpdateEditor(Timestep ts, EditorCamera& camera);
+		void OnViewportResize(uint32_t width, uint32_t height);
+
+		Entity GetPrimaryCameraEntity();
+	private:
+		template<typename T>
+		void OnComponentAdded(Entity entity, T& component);
+	private:
+		entt::registry m_Registry;
+		uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
+
+		b2World* m_PhysicsWorld = nullptr;
+
+		friend class Entity;
+		friend class SceneSerializer;
+		friend class SceneHierarchyPanel;
+	};
+
+}
